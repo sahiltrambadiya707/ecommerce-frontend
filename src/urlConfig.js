50888143c855a1460dd3f7bf2f0a294bfdb04648
sahil <sahiltrambadiya7@gmail.com>
2021-11-29 16:17:14 +0530

    url

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,5 @@
-// const baseUrl = process.env.API || "https://ecommercerestserver.herokuapp.com";
-const baseUrl = "http://localhost:3026";
+const baseUrl = process.env.API || "https://ecommercerestserver.herokuapp.com";
+// const baseUrl = "http://localhost:3026";
 // const baseUrl = "https://ecommercerestserver.herokuapp.com";
 
 export const api = `${baseUrl}/api`;
